8bb5c1996da7eb6227ddbfdba99219772ca2d585
meng.li <dangerouslm1990@gmail.com>
2019-05-31 11:45:16 +0800

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/

--- leetcode/src/main/java/org/dangerous/leetcode/RemoveDuplicatesFromSortedArray.java
+++ leetcode/src/main/java/org/dangerous/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -2,17 +2,18 @@ package org.dangerous.leetcode;
 
 /**
  * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
- * Created by limeng on 17-12-7.
  */
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
         if (nums.length < 2) {
             return nums.length;
         }
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-
+        int index = 0;
+        for (int i = 0; i < nums.length && index < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                nums[++index] = nums[i];
+            }
         }
-        return 0;
+        return index + 1;
     }
 }
