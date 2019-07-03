e1d175fb58ed48d98e24ddb3125929dc18914d94
meng.li <dangerouslm1990@gmail.com>
2019-07-03 18:14:21 +0800

    update lru cache using double linked list and hashmap

--- leetcode/src/main/java/org/dangerous/leetcode/LRUCache.java
+++ leetcode/src/main/java/org/dangerous/leetcode/LRUCache.java
@@ -1,31 +1,83 @@
 package org.dangerous.leetcode;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
  * https://leetcode.com/problems/lru-cache/
- * using with LinkedHashMap
  */
 public class LRUCache {
-    private LinkedHashMap<Integer, Integer> cache;
+    class Node {
+        Node pre, next;
+        int key, value;
+
+        Node() {
+        }
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void removeNode(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    private void addFirst(Node node) {
+        node.next = this.head.next;
+        node.next.pre = node;
+        this.head.next = node;
+        node.pre = this.head;
+    }
+
+    private Node popTail() {
+        Node res = tail.pre;
+        this.removeNode(res);
+        return res;
+    }
+
+    private void move2First(Node node) {
+        this.removeNode(node);
+        this.addFirst(node);
+    }
+
+    private HashMap<Integer, Node> cache;
     private int capacity;
+    private Node head, tail;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        this.cache = new HashMap<>(capacity);
+        this.head = new Node();
+        this.tail = new Node();
+        this.head.next = tail;
+        this.tail.pre = this.head;
     }
 
     public int get(int key) {
-        return cache.getOrDefault(key, -1);
+        Node node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        this.move2First(node);
+        return node.value;
     }
 
     public void put(int key, int value) {
-        cache.put(key, value);
+        Node node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            this.move2First(node);
+            cache.put(key, node);
+            return;
+        }
+        node = new Node(key, value);
+        this.addFirst(node);
+        cache.put(key, node);
+        if (cache.size() > this.capacity) {
+            cache.remove(this.popTail().key);
+        }
     }
+
 }
