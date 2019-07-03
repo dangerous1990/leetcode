92384903e61200798dcc692bc8dacfb24d819073
meng.li <dangerouslm1990@gmail.com>
2019-07-03 11:52:22 +0800

    https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/

--- leetcode/src/main/java/org/dangerous/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
+++ leetcode/src/main/java/org/dangerous/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,20 +1,21 @@
 package org.dangerous.leetcode;
 
+
 import java.util.*;
 
 /**
- * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
  */
-public class BinaryTreeLevelOrderTraversal2 {
+public class BinaryTreeZigzagLevelOrderTraversal {
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         if (root == null) {
             return Collections.emptyList();
         }
         Map<Integer, List<Integer>> m = new HashMap<>(); // key:level value:val in this level
         travel(root, 0, m);
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = m.size()-1; i < m.size(); i--) {
+        for (int i = 0; i < m.size(); i++) {
             result.add(m.get(i));
         }
         return result;
@@ -22,9 +23,13 @@ public class BinaryTreeLevelOrderTraversal2 {
 
     public void travel(TreeNode node, int level, Map<Integer, List<Integer>> m) {
         if (m.containsKey(level)) {
-            m.get(level).add(node.val);
+            if (level % 2 == 0) {
+                ((LinkedList<Integer>) m.get(level)).addLast(node.val);
+            } else {
+                ((LinkedList<Integer>) m.get(level)).addFirst(node.val);
+            }
         } else {
-            List<Integer> l = new ArrayList<>();
+            List<Integer> l = new LinkedList<>();
             l.add(node.val);
             m.put(level, l);
         }
@@ -37,13 +42,16 @@ public class BinaryTreeLevelOrderTraversal2 {
     }
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(10);
+        TreeNode root = new TreeNode(3);
         TreeNode left = new TreeNode(9);
-        TreeNode right = new TreeNode(8);
+        TreeNode right = new TreeNode(20);
+        TreeNode left2 = new TreeNode(15);
         TreeNode right2 = new TreeNode(7);
         root.left = left;
         root.right = right;
         right.right = right2;
-        new BinaryTreeLevelOrderTraversal2().levelOrderBottom(root);
+        right.left = left2;
+        List<List<Integer>> l = new BinaryTreeZigzagLevelOrderTraversal().zigzagLevelOrder(root);
+        System.out.println(l);
     }
 }
