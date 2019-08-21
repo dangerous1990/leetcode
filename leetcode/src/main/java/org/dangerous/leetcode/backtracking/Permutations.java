5dfccfabf880457f09a51f74c43e3d6a23d5a450
meng.li <dangerouslm1990@gmail.com>
2019-08-21 11:34:20 +0800

    https://leetcode.com/problems/permutations/

--- leetcode/src/main/java/org/dangerous/leetcode/backtracking/Permutations.java
+++ leetcode/src/main/java/org/dangerous/leetcode/backtracking/Permutations.java
@@ -1,6 +1,8 @@
 package org.dangerous.leetcode.backtracking;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * https://leetcode.com/problems/permutations/
@@ -8,28 +10,26 @@ import java.util.*;
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new LinkedList<>();
-        backtrace(0,nums,result,new ArrayList<>(),new HashSet<>());
+        backtrace(0, nums, result, new ArrayList<>());
         return result;
     }
 
-    public void backtrace(int index, int[] nums, List<List<Integer>> result, List<Integer> path, Set<Integer> uniq) {
+    public void backtrace(int index, int[] nums, List<List<Integer>> result, List<Integer> path) {
         if (index == nums.length) {
             result.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (uniq.contains(nums[i])){
+            if (path.contains(nums[i])) {
                 continue;
             }
             path.add(nums[i]);
-            uniq.add(nums[i]);
-            backtrace(index + 1, nums, result, path, uniq);
-            uniq.remove(nums[i]);
+            backtrace(index + 1, nums, result, path);
             path.remove(path.size() - 1);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new Permutations().permute(new int[]{1,2,3}));
+        System.out.println(new Permutations().permute(new int[]{1, 2, 3}));
     }
 }
