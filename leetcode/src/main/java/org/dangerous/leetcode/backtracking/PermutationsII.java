086f4b78436dd618c782a71d5f911c3d35355b82
meng.li <dangerouslm1990@gmail.com>
2019-08-26 16:56:48 +0800

    https://leetcode.com/problems/permutations-ii/

--- leetcode/src/main/java/org/dangerous/leetcode/backtracking/PermutationsII.java
+++ leetcode/src/main/java/org/dangerous/leetcode/backtracking/PermutationsII.java
@@ -1,8 +1,9 @@
 package org.dangerous.leetcode.backtracking;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/permutations-ii/
@@ -10,28 +11,32 @@ import java.util.List;
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrace(0, nums, result, new ArrayList<>(), new ArrayList<>());
+        backtrace(0, nums, result, new ArrayList<>());
         return result;
     }
 
-    public void backtrace(int index, int[] nums, List<List<Integer>> result, List<Integer> path, List<Integer> indexPath) {
+    public void backtrace(int index, int[] nums, List<List<Integer>> result, List<Integer> path) {
         if (index == nums.length) {
-            if (result.contains(path)) {
-                return;
-            }
             result.add(new ArrayList<>(path));
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (indexPath.contains(i)) {
-                continue;
+        Set<Integer> appeared = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            if (appeared.add(nums[i])) {
+                path.add(nums[i]);
+                swap(nums, index, i);
+                backtrace(index + 1, nums, result, path);
+                path.remove(path.size() - 1);
+                swap(nums, index, i);
             }
-            path.add(nums[i]);
-            indexPath.add(i);
-            backtrace(index + 1, nums, result, path, indexPath);
-            path.remove(path.size() - 1);
-            indexPath.remove(indexPath.size() - 1);
+
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int save = nums[i];
+        nums[i] = nums[j];
+        nums[j] = save;
     }
 
     public static void main(String[] args) {
