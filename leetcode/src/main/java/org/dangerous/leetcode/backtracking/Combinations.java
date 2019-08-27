2fb65e9840ee3030dadb43147ee26e059baa0c97
meng.li <dangerouslm1990@gmail.com>
2019-08-27 16:07:52 +0800

    https://leetcode.com/problems/combinations

---
+++ leetcode/src/main/java/org/dangerous/leetcode/backtracking/Combinations.java
@@ -0,0 +1,37 @@
+package org.dangerous.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/combinations
+ */
+public class Combinations {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrace(res, new ArrayList<>(k), 0, k, n, 0);
+        return res;
+    }
+
+
+    public void backtrace(List<List<Integer>> res, List<Integer> comb, int index, int k, int n, int prevIndex) {
+        if (index > k) {
+            return;
+        }
+        if (index == k) {
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+        for (int i = prevIndex; i < n; i++) {
+            comb.add(i + 1);
+            backtrace(res, comb, index + 1, k, n, i + 1);
+            comb.remove(comb.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Combinations().combine(4, 2));
+
+    }
+}
