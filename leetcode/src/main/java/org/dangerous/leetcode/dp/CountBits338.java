06d1052701a5f7e296385a594dda06dd17ffcf23
meng.li <dangerouslm1990@gmail.com>
2019-09-10 17:49:13 +0800

    https://leetcode.com/problems/counting-bits/

--- leetcode/src/main/java/org/dangerous/leetcode/dp/CountBits338.java
+++ leetcode/src/main/java/org/dangerous/leetcode/dp/CountBits338.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 public class CountBits338 {
     public int[] countBits(int num) {
         int[] f = new int[num + 1];
+        // i/2 i%2
         for (int i = 1; i <= num; i++) f[i] = f[i >> 1] + (i & 1);
         return f;
     }
